ba627c7c72a1158cdc5034e60bb30ad2b9409d4b
PoojanaSuresh-30 <poojana.s@zvky.com>
2025-09-26 16:09:26 +0530

    Auto Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -170,7 +170,7 @@ function Home()
               <div className="site">
 
                 <div>
-                    <img src={pana} alt="sitefooter" />
+                    <img src={pana} alt="sitefooter" className="sitefooter"  />
                 </div>
 
                 <div>
@@ -186,12 +186,12 @@ function Home()
 
 
             <div className="customers">
-                <div>
+                <div className="cimage">
                <img src={customers} alt="customers" />
 
                 </div>
 
-                <div>
+                <div className="cust">
                     <p>Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.</p>
                     <h3>Tim Smith</h3>
                     <p>British Dragon Boat Racing Association</p>
